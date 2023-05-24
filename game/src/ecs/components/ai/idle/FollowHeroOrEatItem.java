0a125b9d645092a0307f6e32f034d9d5e2528c05
MatteoXAntoz <108668752+MatteoXAntoz@users.noreply.github.com>
2023-05-24 21:13:09 +0200

    Bug Fixes und JavaDoc für FollowHeroOrEatItem
    Bug: es wurde durchgehend die Info ausgegeben
    JavaDoc wurde implementiert.

--- game/src/ecs/components/ai/idle/FollowHeroOrEatItem.java
+++ game/src/ecs/components/ai/idle/FollowHeroOrEatItem.java
@@ -1,6 +1,5 @@
 package ecs.components.ai.idle;
 
-
 import com.badlogic.gdx.ai.pfa.GraphPath;
 import ecs.components.VelocityComponent;
 import ecs.components.ai.AITools;
@@ -10,10 +9,10 @@ import level.elements.tile.Tile;
 import starter.Game;
 import tools.Point;
 
-
-
+/**
+ * Eine Implementierung des IIdleAI-Interfaces, die das Verhalten steuert, wenn die Entität dem Helden folgt oder ein Item frisst.
+ */
 public class FollowHeroOrEatItem implements IIdleAI {
-
 
     public Point from;
     public Point toRandomItem;
@@ -23,57 +22,57 @@ public class FollowHeroOrEatItem implements IIdleAI {
     Entity entity;
     int action;
 
+    boolean stopped;
 
+    /**
+     * Erzeugt eine neue Instanz von FollowHeroOrEatItem für die angegebene Entität.
+     *
+     * @param entity Die Entität, für die das Verhalten gesteuert wird.
+     */
     public FollowHeroOrEatItem(Entity entity){
         this.entity = entity;
-         action = (int) (Math.random()*2+1);
-         item = Game.items.get((int) (Math.random()*Game.items.size()-1));
-         toRandomItem =  item.positionComponent.getPosition();
-
-
+        action = (int) (Math.random()*2+1);
+        item = Game.items.get((int) (Math.random()*Game.items.size()-1));
+        toRandomItem = item.positionComponent.getPosition();
     }
 
     @Override
     public void idle(Entity entity) {
-
-        if(action==1){
+        if(action == 1) {
             walkToPlayer(entity);
-        } else if (action==2) {
+        } else if (action == 2) {
             walkToItem(entity);
-
         }
-
-
     }
 
+    /**
+     * Lässt die Entität dem Helden folgen.
+     *
+     * @param entity Die Entität, die dem Helden folgt.
+     */
     private void walkToPlayer(Entity entity) {
-        if(goToPlayer==null){
+        if(goToPlayer == null) {
             goToPlayer = AITools.calculatePathToHero(entity);
-
         }
-        AITools.move(entity,goToPlayer);
-
-
-
+        AITools.move(entity, goToPlayer);
     }
 
+    /**
+     * Lässt die Entität zu einem zufälligen Item gehen und es fressen.
+     *
+     * @param entity Die Entität, die zu einem Item geht und es frisst.
+     */
     private void walkToItem(Entity entity) {
-        if(eatItems==null){
-            eatItems = AITools.calculatePath(from,toRandomItem);
-
+        if(eatItems == null) {
+            eatItems = AITools.calculatePath(from, toRandomItem);
         }
-        AITools.move(entity,eatItems);
-        if(entity.getComponent(VelocityComponent.class).isPresent()){
+        AITools.move(entity, eatItems);
+        if(entity.getComponent(VelocityComponent.class).isPresent()) {
             VelocityComponent velocityComponent = (VelocityComponent) entity.getComponent(VelocityComponent.class).get();
-            if(velocityComponent.getCurrentXVelocity() == 0&& velocityComponent.getCurrentYVelocity() ==0){
-               Game.removeEntity(item);
+            if(velocityComponent.getCurrentXVelocity() == 0 && velocityComponent.getCurrentYVelocity() == 0 && !stopped) {
+                stopped = true;
+                Game.removeEntity(item);
             }
-
         }
-
-
-
     }
-
-
 }
