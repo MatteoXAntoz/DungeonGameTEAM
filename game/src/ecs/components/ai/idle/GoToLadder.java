7ae1ce7766cc77278a40ccc05d8029ddfd0b234b
MatteoXAntoz <108668752+MatteoXAntoz@users.noreply.github.com>
2023-06-05 23:35:46 +0200

    Klasse wurde dokumentiert und getters und setters
    GoToLadder wurde dokumentiert.
    Getters und Setters wurden verwendet.

--- game/src/ecs/components/ai/idle/GoToLadder.java
+++ game/src/ecs/components/ai/idle/GoToLadder.java
@@ -12,83 +12,136 @@ import level.tools.LevelElement;
 import starter.Game;
 import tools.Constants;
 import tools.Point;
+import tools.Timer;
 
 import javax.swing.plaf.PanelUI;
+import java.awt.event.ItemEvent;
+
+import static starter.Game.currentLevel;
 
 public class GoToLadder implements IIdleAI {
 
-    int coolDownInFrames;
-    int currentCoolDownInFrames;
-    boolean ladderReached;
+    /**
+     * grapLadderPath is the path to the Ladder
+     */
     public GraphPath<Tile> graphLadderPath;
+    /**
+     * graphRandomPoint is the path to a random Point in the dungeon
+     */
     public GraphPath<Tile> graphRandomPoint;
-    Point randomPoint;
-    Tile randomTile;
+    // timer is used for the cooldown of the paths
+    private Timer timer = new Timer();
+    // mode is initialised with R for, which means, that the first path is a random Point
+    private Character mode = 'R';
+    private Point randomPoint;
 
-    PatrouilleWalk patrouilleWalk;
+    //
+    private Tile ladderTile;
 
 
     public GoToLadder() {
-        randomTile = Game.currentLevel.getExitTiles().get(0);
-        randomPoint = Game.currentLevel.getRandomFloorTile().getCoordinateAsPoint();
-        setCoolDown(10);
-        activateCoolDown();
-    }
-
-
-    @Override
-    public void idle(Entity entity) {
-
-        if(isOnCoolDown()){
-            graphRandomPoint = null;
-            if (graphLadderPath == null) {
-                graphLadderPath = AITools.calculatePath(entity.positionComponent.getPosition(),
-                    randomTile.getCoordinate().toPoint());
-            }
-            AITools.move(entity, graphLadderPath);
-            reduceCoolDown();
-
-            reduceCoolDown();
-        }else {
-            graphLadderPath = null;
-            if (graphRandomPoint == null) {
-                graphRandomPoint = AITools.calculatePath(entity.positionComponent.getPosition(),
-                    randomPoint);
-            }
-            AITools.move(entity, graphRandomPoint);
-            if(AITools.pathFinished(entity,graphRandomPoint)){
-                randomPoint = Game.currentLevel.getRandomFloorTile().getCoordinateAsPoint();
-                setCoolDown(10);
-                activateCoolDown();
-            }
-        }
-
-
-
-
-    }
-
-
-    public void setCoolDown(float coolDownInSeconds) {
-        this.coolDownInFrames = (int) (coolDownInSeconds * Constants.FRAME_RATE);
-        this.currentCoolDownInFrames = 0;
-
-    }
-
-    public boolean isOnCoolDown() {
-        System.out.println(currentCoolDownInFrames);
-        return currentCoolDownInFrames > 0;
+        setLadderTile(currentLevel.getExitTiles().get(0));
+        setRandomPoint(currentLevel.getRandomFloorTile().getCoordinateAsPoint());
+        getTimer().setDuration(4);
     }
 
     /**
-     * activate cool down
+     * {@inheritDoc}
+     *
+     * @param entity associated entity
      */
-    public void activateCoolDown() {
-        currentCoolDownInFrames = coolDownInFrames;
+    @Override
+    public void idle(Entity entity) {
+
+        if (!getTimer().isCounting()) {
+            if (getMode().equals('L')) {
+                setMode('R');
+            } else if (getMode().equals('R')) {
+                setMode('L');
+            }
+            getTimer().activateCounter();
+        } else {
+            move(entity, getMode());
+        }
+
+        getTimer().reduceCounter();
+
+
     }
 
-    public void reduceCoolDown() {
-        currentCoolDownInFrames = Math.max(0, --currentCoolDownInFrames);
+
+    // random Point and the path to the Ladder is set up as cycle
+    private void move(Entity entity, Character mode) {
+        if (mode.equals('L')) {
+            if (getRandomPoint() == null) {
+                setGraphRandomPoint(AITools.calculatePath(entity.positionComponent.getPosition(), randomPoint));
+            }
+            AITools.move(entity, getGraphRandomPoint());
+            setRandomPoint(currentLevel.getRandomFloorTile().getCoordinateAsPoint());
+            setGraphLadderPath(null);
+
+        }
+        if (mode.equals('R')) {
+            if (getGraphLadderPath() == null) {
+                setGraphLadderPath(AITools.calculatePath(entity.positionComponent.getPosition()
+                    , getLadderTile().getCoordinate().toPoint()));
+            }
+            AITools.move(entity, getGraphLadderPath());
+            setGraphRandomPoint(null);
+
+
+        }
+
+
     }
 
+    public GraphPath<Tile> getGraphLadderPath() {
+        return graphLadderPath;
+    }
+
+    public void setGraphLadderPath(GraphPath<Tile> graphLadderPath) {
+        this.graphLadderPath = graphLadderPath;
+    }
+
+    public GraphPath<Tile> getGraphRandomPoint() {
+        return graphRandomPoint;
+    }
+
+    public void setGraphRandomPoint(GraphPath<Tile> graphRandomPoint) {
+        this.graphRandomPoint = graphRandomPoint;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+    public Character getMode() {
+        return mode;
+    }
+
+    public void setMode(Character mode) {
+        this.mode = mode;
+    }
+
+    public Point getRandomPoint() {
+        return randomPoint;
+    }
+
+    public void setRandomPoint(Point randomPoint) {
+        this.randomPoint = randomPoint;
+    }
+
+    public Tile getLadderTile() {
+        return ladderTile;
+    }
+
+    public void setLadderTile(Tile ladderTile) {
+        this.ladderTile = ladderTile;
+    }
 }
+
+
