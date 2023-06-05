1e371214ec8cf1f204d9d4181ccdd1439f8b33bd
MatteoXAntoz <108668752+MatteoXAntoz@users.noreply.github.com>
2023-06-05 23:04:31 +0200

    Skill: TeleportSkill
    TeleportSkill für das Teleportieren wurde implementiert.

---
+++ game/src/ecs/components/skill/TeleportSkill.java
@@ -0,0 +1,29 @@
+package ecs.components.skill;
+
+import ecs.components.PositionComponent;
+import ecs.entities.Entity;
+import tools.Point;
+
+public class TeleportSkill extends Skill{
+    /**
+     * @param skillFunction     Function of this skill
+     * @param coolDownInSeconds
+     */
+    public TeleportSkill(ISkillFunction skillFunction, float coolDownInSeconds) {
+        super(skillFunction, coolDownInSeconds);
+    }
+    public void teleport(Entity from,Point to){
+      if(from.getComponent(PositionComponent.class).isPresent()){
+          PositionComponent positionComponent = (PositionComponent) from.getComponent(PositionComponent.class).get();
+          positionComponent.setPosition(to.toCoordinate().toPoint());
+      }
+    }
+
+    public void teleport(Entity from, Entity to){
+        if(from.getComponent(PositionComponent.class).isPresent() && to.getComponent(PositionComponent.class).isPresent()){
+            PositionComponent positionComponentFrom = (PositionComponent) from.getComponent(PositionComponent.class).get();
+            PositionComponent positionComponentTo= (PositionComponent) from.getComponent(PositionComponent.class).get();
+            positionComponentFrom.setPosition(positionComponentTo.getPosition());
+        }
+    }
+}
