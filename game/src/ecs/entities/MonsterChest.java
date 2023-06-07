3857f6c916c887fb14117d1357e30f104117d50f
moritzluetzkendorf <131688857+moritzluetzkendorf@users.noreply.github.com>
2023-06-07 23:59:43 +0200

    Add Javadoc in class MonsterChest

--- game/src/ecs/entities/MonsterChest.java
+++ game/src/ecs/entities/MonsterChest.java
@@ -1,6 +1,7 @@
 package ecs.entities;
 
 
+import com.badlogic.gdx.ai.Logger;
 import com.badlogic.gdx.ai.pfa.GraphPath;
 import dslToGame.AnimationBuilder;
 import ecs.components.*;
@@ -15,14 +16,15 @@ import graphic.Animation;
 import level.elements.tile.Tile;
 import starter.Game;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import  java.util.logging.Logger;
 
 /**
  * class to create a monsterchest that attacks and follows the player
  */
 public class MonsterChest extends Monster {
-
     IFightAI iFightAI;
     ITransition itransition;
     Animation idle, idleLeft, idleRight;
@@ -37,41 +39,71 @@ public class MonsterChest extends Monster {
     public static final String monsterChest_closed = "objects/treasurechest/chest_full_open_anim_f0.png";
     public static final List<String> DEFAULT_CLOSED_ANIMATION_FRAMES =
         List.of("objects/treasurechest/chest_full_open_anim_f0.png");
-    public static final List<String> DEFAULT_OPENING_ANIMATION_FRAMES =
+        public static final List<String> DEFAULT_OPENING_ANIMATION_FRAMES =
         List.of(
             "objects/treasurechest/chest_full_open_anim_f0.png",
             "objects/treasurechest/chest_full_open_anim_f1.png",
             "objects/treasurechest/chest_full_open_anim_f2.png",
             "objects/treasurechest/chest_empty_open_anim_f2.png");
 
+    /**
+     * constructor for class MonsterChest
+     */
+    public MonsterChest() {
+        super();
+        setupITransition();
+        setupAnimation();
+        setupInteraction();
+        setupPosition();
+        setupHitbox();
+        setupAI();
+        new VelocityComponent(this, 0.04f, 0.04F, idleLeft, idleRight);
+        setupHealthComponent();
+    }
+
+    /**
+     * setup the position component
+     */
     protected void setupPosition() {
         positionComponent = new PositionComponent(this);
     }
 
     @Override
     protected void setupVelocity() {
-
     }
 
+    /**
+     * setup the animation component
+     */
     protected void setupAnimation() {
         idleRight = AnimationBuilder.buildAnimation(monsterChest_closed);
         idleLeft = AnimationBuilder.buildAnimation(monsterChest_closed);
         new AnimationComponent(this, idleLeft, idleRight);
     }
 
+    /**
+     * setup the interaction component and sets the fight mode if player interacts with monsterchest
+     */
     protected void setupInteraction() {
         interactionComponent = new InteractionComponent(this, 0.5f, false, new IInteraction() {
             @Override
             public void onInteraction(Entity entity) {
+                monsterChest_logger.info("Monster now follows player");
                 fight = true;
             }
         });
     }
 
+    /**
+     * setup the hitbox
+     */
     protected void setupHitbox() {
         HitboxComponent hitboxComponent = new HitboxComponent(this);
     }
 
+    /**
+     * setup the AI
+     */
     protected void setupAI() {
         AIComponent ai = new AIComponent(this, new IFightAI() {
             @Override
@@ -83,13 +115,13 @@ public class MonsterChest extends Monster {
                 if(healthComponent.getCurrentHealthpoints()==0){
                     dropItem();
                 }
-
-
             }
         }, this, itransition);
-
     }
 
+    /**
+     * setup the health component
+     */
     protected void setupHealthComponent() {
         healthComponent = new HealthComponent(
             this,
@@ -100,22 +132,8 @@ public class MonsterChest extends Monster {
         );
     }
 
-    public MonsterChest() {
-        super();
-        setupITransition();
-        setupAnimation();
-        setupInteraction();
-        setupPosition();
-        setupHitbox();
-        setupAI();
-        new VelocityComponent(this, 0.04f, 0.04F, idleLeft, idleRight);
-        setupHealthComponent();
-
-
-    }
-
     /**
-     * method to drops an item if the monsterches dies
+     * method to drops an item if the monsterchest dies
      */
     public void dropItem() {
         Game.removeEntity(this);
@@ -124,16 +142,12 @@ public class MonsterChest extends Monster {
         for(int i = 0;i<anzahl;i++ ){
            drop.add(Item.ranItem());
         }
-
         for(Item item:drop){
             item.positionComponent.setPosition(positionComponent.getPosition().toCoordinate().toPoint());
         }
-
-
     }
 
-
-
+    // method to calculate the path to player and moves the monsterchest in the direction
     private void followHero(Entity entity) {
         if (followHero == null) {
             followHero = AITools.calculatePath(entity.positionComponent.getPosition(), Game.hero.positionComponent.getPosition());
@@ -142,10 +156,8 @@ public class MonsterChest extends Monster {
         followHero  = null;
     }
 
-
     @Override
     public void idle(Entity entity) {
-
     }
 
     private void setupITransition(){
@@ -156,6 +168,4 @@ public class MonsterChest extends Monster {
             }
         };
     }
-
-
 }
