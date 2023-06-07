fa42d67daeaf4ea591402524ebba7a5269492207
MatteoXAntoz <108668752+MatteoXAntoz@users.noreply.github.com>
2023-06-08 01:53:48 +0200

    Implementiere JavaDoc

--- game/src/ecs/entities/BossMonster.java
+++ game/src/ecs/entities/BossMonster.java
@@ -1,6 +1,10 @@
 package ecs.entities;
 
 public abstract class BossMonster extends Monster {
+    /**
+     * Constructs a new instance of the BossMonster class.
+     * This constructor calls the constructor of the superclass (assuming it exists) to perform any necessary initialization.
+     */
     public BossMonster() {
         super();
     }
