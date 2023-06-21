599d988e74025e45c0d4611d331334aaddc68501
moritzluetzkendorf <131688857+moritzluetzkendorf@users.noreply.github.com>
2023-06-21 12:41:01 +0200

    Add do-while loop in method ghostRiddle in class Riddle and change return value from boolean to int

--- game/src/level/riddle/Riddle.java
+++ game/src/level/riddle/Riddle.java
@@ -12,45 +12,77 @@ public class Riddle {
     // Logger for the class Riddle
     private final Logger riddle_logger = Logger.getLogger(this.getClass().getName());
     /** constructor for class Riddle */
-    public Riddle() {
-        ghostRiddle();
-    }
+    public Riddle() {}
 
     /**
      * method to implement a riddle
      *
      * <p>only to use with entity ghost!
      *
-     * @return boolean value if a riddle was solved or not
+     * @return integer value if a riddle was solved or not
+     *     <p>0 means solved, 1 means not solved
      */
-    public boolean ghostRiddle() {
+    public int ghostRiddle() {
         riddle_logger.info("Riddle was loaded.");
-        boolean isSolved = false;
+        // default value, riddle is not solved yet
+        int valueToReturnForRiddle = 1;
+        boolean riddleIsOver = false;
+
         Scanner scanner = new Scanner(System.in);
+
+        do {
+
         System.out.println("How many we are both?");
+
         String input = scanner.nextLine();
+
         if (input.matches("10")) {
+
             System.out.println("You are a worthy hero.");
-            isSolved = true;
+
+            riddle_logger.info("Riddle solved!");
+
+            valueToReturnForRiddle = 0;
+            riddleIsOver = true;
+
+
+
+
         } else if (input.matches("help")) {
+
             System.out.println("\n");
+
             System.out.println("You are on your own. Good Luck.");
-            ghostRiddle();
+
+
         } else {
+
             System.out.println(
                     "Oh, you fool. This dungeon is not for little kids like you. Last chance...");
             String input2 = scanner.nextLine();
+
             if (input2.matches("10")) {
-                isSolved = true;
+
+                riddle_logger.info("Riddle solved!");
+
+                valueToReturnForRiddle = 0;
+                riddleIsOver = true;
+
             } else if (input2.matches("help")) {
-                ghostRiddle();
+
+
             } else {
                 System.out.println("You have to die.");
+                riddle_logger.info("Riddle not solved!");
+                valueToReturnForRiddle = 1;
+                riddleIsOver = true;
 
-                isSolved = false;
             }
         }
-        riddle_logger.info("Riddle solved: " + isSolved + "!");
-        return isSolved;
+        } while (!riddleIsOver);
+        if (valueToReturnForRiddle != 0) {
+            valueToReturnForRiddle = 1;
+        }
+        return valueToReturnForRiddle;
     }
 }
